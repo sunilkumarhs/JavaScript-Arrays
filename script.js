e338b3fee6269a4db6b25b908cf40e7dee782230
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-06-11 08:03:17 +0530

    starting od bank app

--- script.js
+++ script.js
@@ -88,6 +88,62 @@ banks1.forEach(function (value, key, arr) {
   console.log(`${key} : ${value}`);
 });
 
+console.log('Challenge 1');
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const sortedDogsJulia = dogsJulia.slice(1, -2);
+  console.log("Julia's Data");
+  sortedDogsJulia.forEach(function (age, i) {
+    const result =
+      age >= 3
+        ? `Dog number ${i + 1} is an adult, and is ${age} years old`
+        : `Dog number ${i + 1} is still a puppy`;
+    console.log(result);
+  });
+  console.log("Kate's Data");
+  dogsKate.forEach(function (age, i) {
+    const result =
+      age >= 3
+        ? `Dog number ${i + 1} is an adult, and is ${age} years old`
+        : `Dog number ${i + 1} is still a puppy`;
+    console.log(result);
+  });
+  console.log('Overall from both');
+  const dogsAge = sortedDogsJulia.concat(dogsKate);
+  dogsAge.forEach(function (age, i) {
+    const result =
+      age >= 3
+        ? `Dog number ${i + 1} is an adult, and is ${age} years old`
+        : `Dog number ${i + 1} is still a puppy`;
+    console.log(result);
+  });
+};
+
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+// const dogsJulia = [9, 16, 6, 8, 3];
+// const dogsKate = [10, 5, 6, 1, 4];
+checkDogs(dogsJulia, dogsKate);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -95,28 +151,28 @@ banks1.forEach(function (value, key, arr) {
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
   owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
   owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  transactions: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
   owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
+  transactions: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
 };
@@ -157,8 +213,27 @@ const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
+  ['INR', 'Indian Rupee'],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const displayTransactions = function (transactions) {
+  containerMovements.innerHTML = '';
+  transactions.forEach(function (tranc, i) {
+    const type = tranc > 0 ? 'deposit' : 'withdrawal';
+    const trancRow = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${tranc}₹</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', trancRow);
+  });
+};
+
+displayTransactions(account1.transactions);
