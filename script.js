52b0adba1cb82c547133d7bc13ad311949166283
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-06-15 23:26:28 +0530

    Money transfer

--- script.js
+++ script.js
@@ -356,34 +356,34 @@ const displayTransactions = function (transactions) {
   });
 };
 
-displayTransactions(account1.transactions);
+// displayTransactions(account1.transactions);
 
-const totalBalance = function (transactions) {
-  const balance = transactions.reduce((bal, tranc) => bal + tranc, 0);
-  labelBalance.textContent = `${balance} INR`;
+const totalBalance = function (acc) {
+  acc.balance = acc.transactions.reduce((bal, tranc) => bal + tranc, 0);
+  labelBalance.textContent = `${acc.balance} INR`;
 };
-totalBalance(account1.transactions);
+// totalBalance(account1.transactions);
 
-const totalSummaryDisplay = function (transactions) {
-  const totalDeposit = transactions
+const totalSummaryDisplay = function (acc) {
+  const totalDeposit = acc.transactions
     .filter(tranc => tranc > 0)
     .reduce((dep, tranc) => dep + tranc, 0);
   labelSumIn.textContent = `${totalDeposit} ₹`;
 
-  const totalWithdraw = transactions
+  const totalWithdraw = acc.transactions
     .filter(tranc => tranc < 0)
     .reduce((wit, tranc) => wit + tranc, 0);
   labelSumOut.textContent = `${Math.abs(totalWithdraw)} ₹`;
 
-  const totalIntrest = transactions
+  const totalIntrest = acc.transactions
     .filter(tranc => tranc > 0)
-    .map(tranc => (tranc * 1.2) / 100)
+    .map(tranc => (tranc * acc.interestRate) / 100)
     .filter(int => int >= 1)
     .reduce((tot, int) => tot + int, 0);
   labelSumInterest.textContent = `${totalIntrest} ₹`;
 };
 
-totalSummaryDisplay(account1.transactions);
+// totalSummaryDisplay(account1.transactions);
 
 const createusername = function (accs) {
   accs.forEach(function (acc) {
@@ -399,13 +399,13 @@ console.log(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayTransactions(acc.transactions);
 
   // Display balance
-  calcDisplayBalance(acc);
+  totalBalance(acc);
 
   // Display summary
-  calcDisplaySummary(acc);
+  totalSummaryDisplay(acc);
 };
 
 let currentAccount;
@@ -417,7 +417,6 @@ btnLogin.addEventListener('click', function (e) {
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
-  console.log(currentAccount);
 
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
     // Display UI and message
@@ -431,6 +430,25 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginPin.blur();
 
     // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const reciever = accounts.find(acc => acc.username === inputTransferTo.value);
+  const amount = Number(inputTransferAmount.value);
+  inputTransferTo.value = inputTransferAmount.value = '';
+
+  if (
+    amount > 0 &&
+    reciever &&
+    currentAccount.balance > amount &&
+    reciever.username !== currentAccount.username
+  ) {
+    currentAccount.transactions.push(-amount);
+    reciever.transactions.push(amount);
     updateUI(currentAccount);
   }
 });
